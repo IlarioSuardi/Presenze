d73993441d28e5c935b61be8e0d39a4085442833
suardi.22146 <suardi.ilario.studente@itispaleocapa.it>
2026-04-09 08:08:24 +0200

    .

---
+++ GestionePresenze.java
@@ -0,0 +1,5 @@
+import java.util.*;
+
+public class GestionePresenze {
+
+    private HashMap<String, Integer> presenze;
---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+
+        HashMap presenze = new HashMap();
+        presenze
+
+    }
+}
